1e495982121282118deafe29b70e1a12e53c0c90
DIDI-SU <jisoodidi86@gmail.com>
2022-05-26 11:50:33 +0900

    Add: 파일추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import * as React from "react";
-
+import Main from "./main";
+import Layout from "../styles/GlobalStyles";
 const IndexPage = () => {
-  return <main>블로그</main>;
+  return (
+    <Layout>
+      <Main />
+    </Layout>
+  );
 };
 
 export default IndexPage;
---
+++ src/pages/main.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Header from "../components/Header";
+
+const Main = () => {
+  return (
+    <main>
+      <Header />
+      <title>Home Page</title>
+      <h1>Welcome to my Gatsby site!</h1>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </main>
+  );
+};
+
+export default Main;
